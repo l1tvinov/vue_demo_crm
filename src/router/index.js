b8b6af68921e8a2f9ce0c070c2df46d8b8efa1ce
litvinovl-digital <70130791+litvinovl-digital@users.noreply.github.com>
2021-03-02 21:26:54 +0300

    ler 3

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,81 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+// import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home
+    path: "/login",
+    name: "login",
+    meta: {
+      layout: "empty"
+    },
+    component: () => import("../views/Login.vue")
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/register",
+    name: "register",
+    meta: {
+      layout: "empty"
+    },
+    component: () => import("../views/Register.vue")
+  },
+  {
+    path: "/",
+    name: "Home",
+    meta: {
+      layout: "main"
+    },
+    component: () => import("../views/Home.vue")
+  },
+  {
+    path: "/categories",
+    name: "categories",
+    meta: {
+      layout: "main"
+    },
+    component: () => import("../views/Categories.vue")
+  },
+  {
+    path: "/detail/:id",
+    name: "detail",
+    meta: {
+      layout: "main"
+    },
+    component: () => import("../views/Detail.vue")
+  },
+  {
+    path: "/History",
+    name: "History",
+    meta: {
+      layout: "main"
+    },
+    component: () => import("../views/History.vue")
+  },
+  {
+    path: "/Planning",
+    name: "Planning",
+    meta: {
+      layout: "main"
+    },
+    component: () => import("../views/Planning.vue")
+  },
+  {
+    path: "/Record",
+    name: "Record",
+    meta: {
+      layout: "main"
+    },
+    component: () => import("../views/Record.vue")
+  },
+  {
+    path: "/Profile",
+    name: "Profile",
+    meta: {
+      layout: "main"
+    },
+    component: () => import("../views/Profile.vue")
   }
 ];
 
